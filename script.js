9831e94b28c2e55bd4a0d88b1a421be22da8dc3f
PippaGoshorn <pippa.goshorn@gmail.com>
2023-09-22 14:15:27 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const sainsItemInput = document.getElementById('sainsItemInput');
+const aldiItemInput = document.getElementById('aldiItemInput');
+const addSainsItemButton = document.getElementById('addSainsItemButton');
+const addAldiItemButton = document.getElementById('addAldiItemButton');
+const sainsShoppingList = document.getElementById('sainsShoppingList');
+const aldiShoppingList = document.getElementById('aldiShoppingList');
+
+addSainsItemButton.addEventListener('click', addSainsItem);
+addAldiItemButton.addEventListener('click', addAldiItem);
+sainsItemInput.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        addSainsItemButton.click();
+    }
+});
+aldiItemInput.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        addAldiItemButton.click();
+    }
+});
+
+function addSainsItem() {
+    const sainsItemText = sainsItemInput.value.trim();
+    if (sainsItemText !== '') {
+        const sainsListItem = document.createElement('li');
+        sainsListItem.innerHTML = `
+            ${sainsItemText}
+            <button class="removeSainsItemButton"><i class="fa-solid fa-trash-can"></i></button>
+        `;
+
+    const removeSainsButton = sainsListItem.querySelector('.removeSainsItemButton');
+    removeSainsButton.addEventListener('click', () => {
+        sainsShoppingList.removeChild(sainsListItem);
+    });
+
+    sainsShoppingList.appendChild(sainsListItem);
+    sainsItemInput.value = '';
+    }
+}
+
+function addAldiItem() {
+    const aldiItemText = aldiItemInput.value.trim();
+    if (aldiItemText !== '') {
+        const aldiListItem = document.createElement('li');
+        aldiListItem.innerHTML = `
+            ${aldiItemText}
+            <button class="removeAldiItemButton"><i class="fa-solid fa-trash-can"></i></button>
+        `;
+
+    const removeAldiButton = aldiListItem.querySelector('.removeAldiItemButton');
+    removeAldiButton.addEventListener('click', () => {
+        aldiShoppingList.removeChild(aldiListItem);
+    });
+
+    aldiShoppingList.appendChild(aldiListItem);
+    aldiItemInput.value = '';
+    }
+}
